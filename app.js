e33daa58117af3ca4fd42bc54ae2e5f84799c06f
codypearce <outis161@gmail.com>
2018-10-06 23:14:34 -0400

    get random question route and function

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const questions = require("./data/questions");
+const getRandomInt = require("./src/utils/getRandomInt");
 
 app.get("/api/v1/all", (req, res) => {
   res.json(questions);
+});
+
+app.get("/api/v1/random", (req, res) => {
+  const numberOfQuestions = questions.length - 1;
+  const randomNumber = getRandomInt(0, numberOfQuestions);
+  res.json(questions[randomNumber]);
 });
 
 const port = 5000;
