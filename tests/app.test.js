56b6e9315f5995007f327dae1fe4287d3939f5be
codypearce <outis161@gmail.com>
2018-10-11 23:29:37 -0400

    tests for random and individual question

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,10 +3,19 @@ const questions = require("../data/questions");
 const getRandomInt = require("../src/utils/getRandomInt");
 const request = require("supertest");
 
-describe("GET /api/v1/all", () => {
-  test("with valid data", async () => {
+describe("App Tests", () => {
+  test("GET /api/v1/all", async () => {
     const response = await request(app).get("/api/v1/all");
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual(questions);
+  });
+  test("GET /api/v1/random", async () => {
+    const response = await request(app).get("/api/v1/random");
+    expect(response.statusCode).toBe(200);
+  });
+  test("GET /api/v1/:questionId", async () => {
+    const response = await request(app).get("/api/v1/1");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(questions[1]);
   });
 });
