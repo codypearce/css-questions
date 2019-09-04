2d041fd6fceab791e51086c1503783d1f2cb3776
codypearce <codyapearce@gmail.com>
2019-09-04 16:08:55 -0700

    Add question number, card width 100%

--- src/ui/Question.js
+++ src/ui/Question.js
@@ -2,16 +2,19 @@ import React from "react";
 
 const Question = ({ question }) => {
   return (
-    <li className="question">
-      <span className="question__question">{question.question}</span>
-      <span
-        className={`question__answer ${
-          question ? "question__answer--show" : ""
-        }`}
-      >
-        {question.answer}
-      </span>
-    </li>
+    <div className="question">
+      <div className="question__number">{question.position}</div>
+      <li className="question__card">
+        <span className="question__question">{question.question}</span>
+        <span
+          className={`question__answer ${
+            question ? "question__answer--show" : ""
+          }`}
+        >
+          {question.answer}
+        </span>
+      </li>
+    </div>
   );
 };
 
